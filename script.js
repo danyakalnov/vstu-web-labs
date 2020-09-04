014fbf75a68ef6361d4907588e572bcb0764a26d
Danila Kalnov <danyakalnov@gmail.com>
2020-09-04 17:09:17 +0400

    Add functionality to open/hide menu on outside click using event.target

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const start = () => {
-    const myInput = document.querySelector('.sample-input');
-    myInput.addEventListener('input', event => {
+    const myInputs = document.querySelectorAll('.sample-input');
+    myInputs.forEach(input => addEventListener('input', event => {
         console.log(event.target.value);
+    }));
+
+    const menu = document.querySelector('.menu');
+
+    const button = document.querySelector('#button');
+
+    button.addEventListener('click', () => {
+        menu.classList.toggle('menu-show');
+    });
+
+    document.addEventListener('click', event => {
+        const isClickInside = menu.contains(event.target);
+        if (!isClickInside && menu.classList.contains('menu-show') && event.target !== button) {
+            menu.classList.toggle('menu-show');
+        }
     });
 }
 
