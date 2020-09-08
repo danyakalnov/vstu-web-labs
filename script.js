f65bfda3393098a7c471948bb654f17f140e2b8b
Danila Kalnov <danyakalnov@gmail.com>
2020-09-08 20:15:34 +0400

    Add a code to watch for inputs and textarea changes and check if the given input match with specified regexp

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const start = () => {
     const myInputs = document.querySelectorAll('.sample-input');
-    myInputs.forEach(input => addEventListener('input', event => {
-        console.log(event.target.value);
-    }));
 
+    const testInput = (targetInput) => {
+        let testResult = regexp.test(targetInput.value);
+        const resultIndicator = targetInput.nextElementSibling;
+        if (testResult) {
+            resultIndicator.textContent = 'There is a match!';
+            resultIndicator.classList.add('answer');
+            resultIndicator.classList.remove('no-match');
+        }
+        else {
+            resultIndicator.textContent = 'No match :('
+            resultIndicator.classList.add('no-match');
+            resultIndicator.classList.remove('answer');
+        }
+    }
+
+    // Регулярное выражение из textarea
+
+    const regexpInput = document.querySelector('#regexp-input');
+    let regexp = new RegExp(regexpInput.textContent);
+    regexpInput.addEventListener('input', event => {
+        let newRegexp = new RegExp(event.target.value);
+        regexp = newRegexp;
+        regexpInput.textContent = event.target.value;
+        myInputs.forEach(input => {
+            console.log();
+            testInput(input);
+        });
+    });
+    
+    myInputs.forEach(input => input.addEventListener('input', event => {
+        testInput(event.target);
+    }));
+    
     const menu = document.querySelector('.menu');
     const button = document.querySelector('#button');
     const menuText = document.querySelector('.menu-text');
